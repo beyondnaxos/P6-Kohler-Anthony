cd65ee8cf64a1f70180b0ba2e62eb2f549961c3d
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2021-11-30 11:01:20 +0100

    Création du model
    resolution de l'erreur lors du log !

--- back/models/sauce.js
+++ back/models/sauce.js
@@ -1,11 +1,17 @@
 const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
-  title: {  type : String, required : true },
-    description: { type : String, required : true },
-    imageUrl: { type : String, required : true },
-    userId: { type : String, required : true },
-    price: { type : Number, required : true },
+  userId: { type: String, required: true },
+  name: { type: String, required: true },
+  manufacturer: { type: String, required: true },
+  description: { type: String, required: true },
+  mainPepper: { type: String, required: true },
+  imageUrl: { type: String, required: true },
+  heat: { type: Number, required: true },
+  likes: { type: Number, required: true },
+  dislikes: { type: Number, required: true },
+  userLiked: { type: Number, required: true },
+  userDisliked: { type: Number, required: true },
 })
 
 module.exports = mongoose.model('Sauce', sauceSchema);
