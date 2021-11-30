011b2f5982054fd5c934ffd55676a7036996b7f0
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2021-11-30 10:52:54 +0100

    Mise en place de sauce
    erreur 401 login unauthorized

--- back/app.js
+++ back/app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
 
-const stuffRoutes = require('./routes/stuff');
+const sauceRoutes = require('./routes/sauce');
 const userRoutes = require('./routes/user');
 
 mongoose.connect('mongodb+srv://patoch:patoch@cluster0.ob1zz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
@@ -22,7 +22,7 @@ app.use((req, res, next) => {
 
 app.use(express.json());
 app.use('/images', express.static(path.join(__dirname, 'images')));
-app.use('/api/stuff', stuffRoutes);
+app.use('/api/sauce', sauceRoutes);
 app.use('/api/auth', userRoutes);
 
 module.exports = app;
