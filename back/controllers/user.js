0a2a5bda8f41311bcfc49ea9ac296df960dddd66
beyondnaxos <86872427+beyondnaxos@users.noreply.github.com>
2021-12-15 08:53:36 +0100

    correction dotenv ctrlUser

--- back/controllers/user.js
+++ back/controllers/user.js
@@ -1,6 +1,6 @@
 const bcrypt = require("bcrypt") 
 const jwt = require('jsonwebtoken') 
-
+require('dotenv').config()
 const User = require("../models/user") 
 
 exports.signup = (req, res, next) => {
@@ -32,7 +32,7 @@ exports.signup = (req, res, next) => {
               userId: user._id,
               token: jwt.sign(
                   {userId : user._id},
-                  'RANDOM_TOKEN_SECRET',
+                  process.env.JWT_KEY,
                   { expiresIn: '24h' }
               )
             }) 
